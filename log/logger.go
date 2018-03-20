0e9ddfbca17eb08be220f58108db1ce4d0b425d3
Sotirios Mantziaris <smantziaris@gmail.com>
2018-03-20 22:03:20 +0200

    dep initialize, added log tests

---
+++ log/logger.go
@@ -0,0 +1,19 @@
+package log
+
+// Logger interface defines all methods a logger has to implement
+// for this abstraction
+type Logger interface {
+	Fields() map[string]interface{}
+	Fatal(...interface{})
+	Fatalf(string, ...interface{})
+	Panic(...interface{})
+	Panicf(string, ...interface{})
+	Error(...interface{})
+	Errorf(string, ...interface{})
+	Warn(...interface{})
+	Warnf(string, ...interface{})
+	Info(...interface{})
+	Infof(string, ...interface{})
+	Debug(...interface{})
+	Debugf(string, ...interface{})
+}
