1591b8e98f3dbd9a0abac2993acb5493c8aab528
Sotirios Mantziaris <smantziaris@gmail.com>
2018-03-20 21:52:59 +0200

    Added travis, readme and doc

--- log/log.go
+++ log/log.go
@@ -1,7 +1,13 @@
 package log
 
+// Logger interface defines all methods a logger has to implement
+// for this abstraction
 type Logger interface {
 	Fields() map[string]interface{}
+	Fatal(...interface{})
+	Fatalf(string, ...interface{})
+	Panic(...interface{})
+	Panicf(string, ...interface{})
 	Error(...interface{})
 	Errorf(string, ...interface{})
 	Warn(...interface{})
@@ -10,8 +16,4 @@ type Logger interface {
 	Infof(string, ...interface{})
 	Debug(...interface{})
 	Debugf(string, ...interface{})
-	Fatal(...interface{})
-	Fatalf(string, ...interface{})
-	Panic(...interface{})
-	Panicf(string, ...interface{})
 }
