98e21a182f8c343a7ec7a6e0f0ed5c2e850de099
Sotirios Mantziaris <smantziaris@gmail.com>
2018-03-30 00:38:15 +0300

    Added zerolog option (#28)

---
+++ log/zero/option.go
@@ -0,0 +1,19 @@
+package zero
+
+import (
+	"github.com/mantzas/patron/http"
+	"github.com/mantzas/patron/log"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+)
+
+// Log option for setting the zerolog default logging
+func Log(l zerolog.Level) http.Option {
+	return func(s http.Service) error {
+		err := log.Setup(DefaultFactory(l))
+		if err != nil {
+			return errors.Wrap(err, "failed to set up zerolog default")
+		}
+		return nil
+	}
+}
