f4474e42deb8a1b0c9f3a397edbb5b538435eaaf
Sotirios Mantziaris <s.mantziaris@thebeat.co>
2018-06-12 15:59:18 +0300

    Increased test coverage

--- option_test.go
+++ option_test.go
@@ -10,18 +10,20 @@ func TestTracing(t *testing.T) {
 	assert := assert.New(t)
 	type args struct {
 		addr string
+		name string
 	}
 	tests := []struct {
 		name    string
 		args    args
 		wantErr bool
 	}{
-		{"failure due to empty agent address", args{""}, true},
-		{"success", args{"0.0.0.0:6831"}, false},
+		{"failure due to empty agent address", args{"", "TEST"}, true},
+		{"failure due to service name missing", args{"0.0.0.0:6831", ""}, true},
+		{"success", args{"0.0.0.0:6831", "TEST"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := Service{name: "test"}
+			s := Service{name: tt.args.name}
 			err := Tracing(tt.args.addr)(&s)
 			if tt.wantErr {
 				assert.Error(err)
