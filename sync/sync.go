ffecc6fade60ee1b1348d3be4c1ca64b8f2fa0d1
Sotirios Mantziaris <smantziaris@gmail.com>
2018-06-18 22:58:42 +0300

    Refactor errors

--- sync/sync.go
+++ sync/sync.go
@@ -48,6 +48,11 @@ func (e *ValidationError) Error() string {
 	return e.err
 }
 
+// NewValidationError creates a new validation error.
+func NewValidationError(msg string) *ValidationError {
+	return &ValidationError{msg}
+}
+
 // UnauthorizedError defines a authorization error.
 type UnauthorizedError struct {
 	err string
@@ -55,6 +60,11 @@ type UnauthorizedError struct {
 
 func (e *UnauthorizedError) Error() string {
 	return e.err
+}
+
+// NewUnauthorizedError creates a new unauthorized error.
+func NewUnauthorizedError(msg string) *UnauthorizedError {
+	return &UnauthorizedError{msg}
 }
 
 // ForbiddenError defines a access error.
@@ -66,6 +76,11 @@ func (e *ForbiddenError) Error() string {
 	return e.err
 }
 
+// NewForbiddenError creates a new forbidden error.
+func NewForbiddenError(msg string) *ForbiddenError {
+	return &ForbiddenError{msg}
+}
+
 // NotFoundError defines a not found error.
 type NotFoundError struct {
 	err string
@@ -75,6 +90,11 @@ func (e *NotFoundError) Error() string {
 	return e.err
 }
 
+// NewNotFoundError creates a new not found error.
+func NewNotFoundError(msg string) *NotFoundError {
+	return &NotFoundError{msg}
+}
+
 // ServiceUnavailableError defines a service unavailable error.
 type ServiceUnavailableError struct {
 	err string
@@ -82,4 +102,9 @@ type ServiceUnavailableError struct {
 
 func (e *ServiceUnavailableError) Error() string {
 	return e.err
+}
+
+// NewServiceUnavailableError creates a new service unavailable error.
+func NewServiceUnavailableError(msg string) *ServiceUnavailableError {
+	return &ServiceUnavailableError{msg}
 }
