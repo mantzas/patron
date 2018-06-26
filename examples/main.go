4eaaa7944c12f295a4ba1c1e2f3e4d3504e0df6e
Sotirios Mantziaris <smantziaris@gmail.com>
2018-06-26 15:22:05 +0300

    Traced http client fixes #108 (#117)

--- examples/main.go
+++ examples/main.go
@@ -4,25 +4,26 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/mantzas/patron/trace"
 	"github.com/pkg/errors"
 
 	"github.com/mantzas/patron"
 	"github.com/mantzas/patron/log"
 	"github.com/mantzas/patron/sync"
 	sync_http "github.com/mantzas/patron/sync/http"
+	trace_http "github.com/mantzas/patron/trace/http"
 )
 
 func process(ctx context.Context, req *sync.Request) (*sync.Response, error) {
-	sp, ctx := trace.StartChildSpan(ctx, "google-client", "http-client")
-	sp.LogKV("action", "getting www.google.com")
-	rsp, err := http.DefaultClient.Get("https://www.google.com")
+	googleReq, err := http.NewRequest("GET", "https://www.google.com", nil)
 	if err != nil {
-		trace.FinishSpan(sp, true)
-		return nil, errors.Wrap(err, "failed to get google.com")
+		return nil, errors.Wrap(err, "failed to create requestfor www.google.com")
 	}
-	defer trace.FinishSpan(sp, false)
+	rsp, err := trace_http.NewClient(1*time.Second).Do(ctx, googleReq)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get www.google.com")
+	}
 	return sync.NewResponse(fmt.Sprintf("got %s from google", rsp.Status)), nil
 }
 
