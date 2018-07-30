99495cc8239079ad4400d61abfed7726e53d888c
Sotirios Mantziaris <smantziaris@gmail.com>
2018-07-30 23:16:37 +0300

    Added specific route functions

--- examples/main.go
+++ examples/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/mantzas/patron"
-	synchttp "github.com/mantzas/patron/sync/http"
+	"github.com/mantzas/patron/log"
+	"github.com/mantzas/patron/log/zerolog"
+	"github.com/mantzas/patron/sync/http"
 )
 
 // Audit records name and time of a processing step.
@@ -37,46 +38,56 @@ const (
 	kafkaBroker  = "localhost:9092"
 )
 
+var logger log.Logger
+
 func init() {
-	err := os.Setenv("PATRON_LOG_LEVEL", "debug")
+	err := log.Setup(zerolog.DefaultFactory(log.DebugLevel), nil)
 	if err != nil {
-		log.Fatalf("failed to set log level env var: %v", err)
+		fmt.Printf("failed to setup logging: %v", err)
+		os.Exit(1)
+	}
+	logger = log.Create()
+
+	err = os.Setenv("PATRON_LOG_LEVEL", "debug")
+	if err != nil {
+		logger.Fatalf("failed to set log level env var: %v", err)
 
 	}
 	err = os.Setenv("PATRON_JAEGER_SAMPLER_PARAM", "1.0")
 	if err != nil {
-		log.Fatalf("failed to set sampler env vars:: %v", err)
+		logger.Fatalf("failed to set sampler env vars:: %v", err)
 	}
 }
 
 func main() {
-
 	amqpCmp, err := newAmqpComponent("amqp consumer", amqpURL, amqpQueue, amqpExchange)
 	if err != nil {
-		log.Fatalf("failed to create processor %v", err)
+		logger.Fatalf("failed to create processor %v", err)
 	}
 
 	kafkaCmp, err := newKafkaComponent("kafka consumer", kafkaBroker, kafkaTopic, amqpURL, amqpExchange)
 	if err != nil {
-		log.Fatalf("failed to create processor %v", err)
+		logger.Fatalf("failed to create processor %v", err)
 	}
 
 	httpCmp, err := newHTTPComponent(kafkaBroker, kafkaTopic)
 	if err != nil {
-		log.Fatalf("failed to create processor %v", err)
+		logger.Fatalf("failed to create processor %v", err)
 	}
 
 	// Set up routes
-	routes := make([]synchttp.Route, 0)
-	routes = append(routes, synchttp.NewRoute("/", http.MethodPost, httpCmp.process, true))
+	routes := []http.Route{
+		http.NewPostRoute("/", httpCmp.process, true),
+	}
 
 	srv, err := patron.New("patron", "1.0.0", patron.Routes(routes), patron.Components(kafkaCmp.cmp, amqpCmp.cmp))
 	if err != nil {
-		log.Fatalf("failed to create service %v", err)
+		logger.Fatalf("failed to create service %v", err)
 	}
 
 	err = srv.Run()
 	if err != nil {
-		log.Fatalf("failed to create service %v", err)
+		logger.Fatalf("failed to create service %v", err)
 	}
+
 }
--- sync/http/route_test.go
+++ sync/http/route_test.go
@@ -11,10 +11,41 @@ func TestNewRoute(t *testing.T) {
 	assert := assert.New(t)
 	r := NewRoute("/index", http.MethodGet, nil, true)
 	assert.Equal("/index", r.Pattern)
-	assert.Equal("GET", r.Method)
+	assert.Equal(http.MethodGet, r.Method)
 	assert.True(r.Trace)
 }
 
+func TestNewGetRoute(t *testing.T) {
+	assert := assert.New(t)
+	r := NewGetRoute("/index", nil, true)
+	assert.Equal("/index", r.Pattern)
+	assert.Equal(http.MethodGet, r.Method)
+	assert.True(r.Trace)
+}
+
+func TestNewPostRoute(t *testing.T) {
+	assert := assert.New(t)
+	r := NewPostRoute("/index", nil, true)
+	assert.Equal("/index", r.Pattern)
+	assert.Equal(http.MethodPost, r.Method)
+	assert.True(r.Trace)
+}
+
+func TestNewPutRoute(t *testing.T) {
+	assert := assert.New(t)
+	r := NewPutRoute("/index", nil, true)
+	assert.Equal("/index", r.Pattern)
+	assert.Equal(http.MethodPut, r.Method)
+	assert.True(r.Trace)
+}
+
+func TestNewDeleteRoute(t *testing.T) {
+	assert := assert.New(t)
+	r := NewDeleteRoute("/index", nil, true)
+	assert.Equal("/index", r.Pattern)
+	assert.Equal(http.MethodDelete, r.Method)
+	assert.True(r.Trace)
+}
 func TestNewRouteRaw(t *testing.T) {
 	assert := assert.New(t)
 	r := NewRouteRaw("/index", http.MethodGet, nil)
