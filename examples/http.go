1bd41895abda2e28aaffb209cdf3d749a44cea5d
Sotirios Mantziaris <smantziaris@gmail.com>
2018-08-05 12:13:13 +0300

    Added http client tracing example

--- examples/http.go
+++ examples/http.go
@@ -15,19 +15,38 @@ import (
 type httpComponent struct {
 	prd   kafka.Producer
 	topic string
+	url   string
 }
 
-func newHTTPComponent(kafkabroker, topic string) (*httpComponent, error) {
+func newHTTPComponent(kafkabroker, topic, url string) (*httpComponent, error) {
 
 	prd, err := kafka.NewAsyncProducer([]string{kafkaBroker})
 	if err != nil {
 		return nil, err
 	}
-	return &httpComponent{prd: prd, topic: topic}, nil
+	return &httpComponent{prd: prd, topic: topic, url: url}, nil
 }
 
-func (hc *httpComponent) process(ctx context.Context, req *sync.Request) (*sync.Response, error) {
-	aud := Audit{Name: "HTTP component", Started: time.Now()}
+func (hc *httpComponent) first(ctx context.Context, req *sync.Request) (*sync.Response, error) {
+	aud := Audit{Name: "first HTTP component", Started: time.Now()}
+	secondRouteReq, err := http.NewRequest("GET", hc.url, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create requestfor www.google.com")
+	}
+	secondRouteReq.Header.Add("Content-Type", "application/json")
+	rsp, err := tracehttp.NewClient(5*time.Second).Do(ctx, secondRouteReq)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get www.google.com")
+	}
+
+	ads := Audits{}
+	ads.append(aud)
+
+	return sync.NewResponse(fmt.Sprintf("got %s from second HTTP route", rsp.Status)), nil
+}
+
+func (hc *httpComponent) second(ctx context.Context, req *sync.Request) (*sync.Response, error) {
+	aud := Audit{Name: "second HTTP component", Started: time.Now()}
 	googleReq, err := http.NewRequest("GET", "https://www.google.com", nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create requestfor www.google.com")
