4b444db88c574b2a5da048e523694fbce1ed3de5
Sotirios Mantziaris <smantziaris@gmail.com>
2018-09-18 22:39:18 +0300

    Added readme,dockerfile etc
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- cmd/patron-cli/main.go
+++ cmd/patron-cli/main.go
@@ -8,12 +8,19 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+)
+
+const (
+	nameTemplate    = "{{name}}"
+	versionTemplate = "{{version}}"
 )
 
 func main() {
 	module := flag.String("m", "", `define the module name ("github.com/mantzas/patron")`)
+	version := flag.String("v", "1.0.0", "define the version")
 	path := flag.String("p", "", "define the project folder (defaults to current)")
-	vendor := flag.Bool("v", true, "define vendoring behavior")
+	vendor := flag.Bool("d", true, "define vendoring behavior")
 	flag.Parse()
 
 	if *path == "" {
@@ -28,6 +35,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	name := nameFromModule(*module)
+	log.Printf("assuming name: %s", name)
+
 	err := createPathAndChdir(*path)
 	if err != nil {
 		log.Fatalf("failed to create path: %v", err)
@@ -38,16 +48,20 @@ func main() {
 		log.Fatalf("failed to create git: %v", err)
 	}
 
-	err = createMain()
+	err = createMain(name, *version)
 	if err != nil {
 		log.Fatalf("failed to create main: %v", err)
 	}
 
-	// Create docker file
+	err = createDockerfile(name)
+	if err != nil {
+		log.Fatalf("failed to create Dockerfile: %v", err)
+	}
 
-	// Create dashboard folder and files
-
-	// create readme.md
+	err = createReadme(name)
+	if err != nil {
+		log.Fatalf("failed to create README.md: %v", err)
+	}
 
 	err = goMod(*module, *vendor)
 	if err != nil {
@@ -66,20 +80,39 @@ func main() {
 	log.Print("completed successful")
 }
 
+func nameFromModule(module string) string {
+	lst := strings.LastIndex(module, "/")
+	if lst == -1 {
+		return module
+	}
+
+	return module[lst+1:]
+}
+
 func setupGit() error {
 	log.Printf("creating git repository")
 	return exec.Command("git", "init").Run()
 }
 
+func createDockerfile(name string) error {
+	return ioutil.WriteFile("Dockerfile", dockerfileContent(name), 0664)
+}
+
+func createReadme(name string) error {
+	return ioutil.WriteFile("README.md", readmeContent(name), 0664)
+}
+
 func goMod(module string, vendor bool) error {
 	log.Print("setup go module support")
 	out, err := exec.Command("go", "mod", "init", module).CombinedOutput()
+	log.Print(string(out))
 	if err != nil {
 		return errors.New(string(out))
 	}
 	if vendor {
 		log.Print("add vendoring")
 		out, err := exec.Command("go", "mod", "vendor").CombinedOutput()
+		log.Print(string(out))
 		if err != nil {
 			return errors.New(string(out))
 		}
@@ -88,25 +121,27 @@ func goMod(module string, vendor bool) error {
 }
 
 func createPathAndChdir(path string) error {
-	log.Printf("create %s folder", path)
+	log.Printf("create folder: %s", path)
 	err := os.MkdirAll(path, 0775)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("cd to %s", path)
+	log.Printf("cd into: %s", path)
 	return os.Chdir(path)
 }
 
-func createMain() error {
-	log.Print("create cmd folder")
-	err := os.MkdirAll("cmd", 0775)
+func createMain(name, version string) error {
+	folder := fmt.Sprintf("cmd/%s", name)
+	log.Printf("create folder: %s", folder)
+	err := os.MkdirAll(folder, 0775)
 	if err != nil {
 		return err
 	}
 
-	log.Print("create main.go")
-	return ioutil.WriteFile("cmd/main.go", mainContent(), 0664)
+	file := fmt.Sprintf("%s/main.go", folder)
+	log.Printf("create file: %s", file)
+	return ioutil.WriteFile(file, mainContent(name, version), 0664)
 }
 
 func gitCommit() error {
@@ -128,8 +163,20 @@ func goFormat() error {
 	return nil
 }
 
-func mainContent() []byte {
-	return []byte(`package main
+func dockerfileContent(name string) []byte {
+	cnt := `FROM golang:latest as builder
+COPY . ./
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o {{name}} ./cmd/{{name}}/main.go 
+
+FROM scratch
+COPY --from=builder {{name}} .
+CMD ["./{{name}}"]
+`
+	return []byte(strings.Replace(cnt, nameTemplate, name, -1))
+}
+
+func mainContent(name, version string) []byte {
+	cnt := `package main
 
 import (
 	"fmt"
@@ -140,8 +187,8 @@ import (
 )
 
 func main() {
-	name := "patron"
-	version := "1.0.0"
+	name := "{{name}}"
+	version := "{{version}}"
 
 	err := patron.SetupLogging(name, version)
 	if err != nil {
@@ -159,5 +206,13 @@ func main() {
 		log.Fatalf("failed to run service %v", err)
 	}
 }
-`)
+`
+	cnt = strings.Replace(cnt, nameTemplate, name, -1)
+	cnt = strings.Replace(cnt, versionTemplate, version, -1)
+	return []byte(cnt)
+}
+
+func readmeContent(name string) []byte {
+	cnt := `# {{name}}`
+	return []byte(strings.Replace(cnt, nameTemplate, name, -1))
 }
