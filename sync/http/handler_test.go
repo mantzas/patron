d1c695987907a47ffafe13aa01072d536b329830
Sotirios Mantziaris <smantziaris@gmail.com>
2019-01-27 00:00:30 +0200

    Linting
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- sync/http/handler_test.go
+++ sync/http/handler_test.go
@@ -202,14 +202,46 @@ func Test_handleError(t *testing.T) {
 		args         args
 		expectedCode int
 	}{
-		{"bad request", args{err: NewValidationError(), enc: json.Encode}, http.StatusBadRequest},
-		{"unauthorized request", args{err: NewUnauthorizedError(), enc: json.Encode}, http.StatusUnauthorized},
-		{"forbidden request", args{err: NewForbiddenError(), enc: json.Encode}, http.StatusForbidden},
-		{"not found error", args{err: NewNotFoundError(), enc: json.Encode}, http.StatusNotFound},
-		{"service unavailable error", args{err: NewServiceUnavailableError(), enc: json.Encode}, http.StatusServiceUnavailable},
-		{"internal server error", args{err: NewError(), enc: json.Encode}, http.StatusInternalServerError},
-		{"default error", args{err: errors.New("Test"), enc: json.Encode}, http.StatusInternalServerError},
-		{"payload encoding error", args{err: NewErrorWithCodeAndPayload(http.StatusBadRequest, make(chan int)), enc: json.Encode}, http.StatusInternalServerError},
+		{
+			name:         "bad request",
+			args:         args{err: NewValidationError(), enc: json.Encode},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "unauthorized request",
+			args:         args{err: NewUnauthorizedError(), enc: json.Encode},
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name:         "forbidden request",
+			args:         args{err: NewForbiddenError(), enc: json.Encode},
+			expectedCode: http.StatusForbidden,
+		},
+		{
+			name:         "not found error",
+			args:         args{err: NewNotFoundError(), enc: json.Encode},
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:         "service unavailable error",
+			args:         args{err: NewServiceUnavailableError(), enc: json.Encode},
+			expectedCode: http.StatusServiceUnavailable,
+		},
+		{
+			name:         "internal server error",
+			args:         args{err: NewError(), enc: json.Encode},
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "default error",
+			args:         args{err: errors.New("Test"), enc: json.Encode},
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name:         "payload encoding error",
+			args:         args{err: NewErrorWithCodeAndPayload(http.StatusBadRequest, make(chan int)), enc: json.Encode},
+			expectedCode: http.StatusInternalServerError,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
