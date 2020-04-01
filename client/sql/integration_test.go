2e5b02c99517280f7420557e74a095340d691b4a
Vangelis Katikaridis <drakos74@gmail.com>
2020-04-01 20:08:50 +0200

    Introduce dockertest to integration tests (#182)
    Signed-off-by: Vangelis Katikaridis <v.katikaridis@thebeat.co>

--- client/sql/integration_test.go
+++ client/sql/integration_test.go
@@ -4,14 +4,54 @@ package sql
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/beatlabs/patron/log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/mocktracer"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	dbHost           = "localhost"
+	dbSchema         = "patrondb"
+	dbPort           = "3309"
+	dbRouterPort     = "33069"
+	dbPassword       = "test123"
+	dbRootPassword   = "test123"
+	dbUsername       = "patron"
+	connectionFormat = "%s:%s@(%s:%s)/%s?parseTime=true"
+)
+
+func TestMain(m *testing.M) {
+
+	d := dockerRuntime{}
+
+	err := d.startUpContainerSync()
+	if err != nil {
+		log.Errorf("could not start containers %v", err)
+		os.Exit(1)
+	}
+
+	exitVal := m.Run()
+
+	err = d.tearDownContainerSync()
+	if err != nil {
+		log.Errorf("could not tear down containers %v", err)
+		os.Exit(1)
+	}
+
+	os.Exit(exitVal)
+}
 
 func TestOpen(t *testing.T) {
 	type args struct {
@@ -26,7 +66,7 @@ func TestOpen(t *testing.T) {
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			got, err := Open(tt.args.driverName, "patron:test123@tcp/patrondb?parseTime=true")
+			got, err := Open(tt.args.driverName, fmt.Sprintf(connectionFormat, dbUsername, dbPassword, dbHost, dbPort, dbSchema))
 
 			if tt.expectedErr != "" {
 				assert.EqualError(t, err, tt.expectedErr)
@@ -47,7 +87,7 @@ func TestIntegration(t *testing.T) {
 	const query = "SELECT * FROM employee LIMIT 1"
 	const insertQuery = "INSERT INTO employee(name) value (?)"
 
-	db, err := Open("mysql", "patron:test123@tcp(127.0.0.1:3307)/patrondb?parseTime=true")
+	db, err := Open("mysql", fmt.Sprintf(connectionFormat, dbUsername, dbPassword, dbHost, dbPort, dbSchema))
 	assert.NoError(t, err)
 	assert.NotNil(t, db)
 	db.SetConnMaxLifetime(time.Minute)
@@ -287,4 +327,78 @@ func assertSpan(t *testing.T, sp *mocktracer.MockSpan, opName, statement string)
 		"version":      "dev",
 		"error":        false,
 	}, sp.Tags())
+}
+
+type dockerRuntime struct {
+	sql  *dockertest.Resource
+	pool *dockertest.Pool
+}
+
+func (d *dockerRuntime) startUpContainerSync() error {
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		return err
+	}
+	d.pool = pool
+	d.pool.MaxWait = time.Minute * 2
+
+	d.sql, err = d.pool.RunWithOptions(&dockertest.RunOptions{Repository: "mysql",
+		Tag: "5.7.25",
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"3306/tcp":  {{HostIP: "", HostPort: dbPort}},
+			"33060/tcp": {{HostIP: "", HostPort: dbRouterPort}},
+		},
+		ExposedPorts: []string{"3306/tcp", "33060/tcp"},
+		Env: []string{
+			fmt.Sprintf("MYSQL_ROOT_PASSWORD=%s", dbRootPassword),
+			fmt.Sprintf("MYSQL_USER=%s", dbUsername),
+			fmt.Sprintf("MYSQL_PASSWORD=%s", dbPassword),
+			fmt.Sprintf("MYSQL_DATABASE=%s", dbSchema),
+			"TIMEZONE=UTC",
+		}})
+	if err != nil {
+		return err
+	}
+
+	// optionally print the container logs in stdout
+	d.tailLogs(d.sql.Container.ID, os.Stdout)
+
+	// wait until the container is ready
+	return d.pool.Retry(func() error {
+		db, err := sql.Open("mysql", fmt.Sprintf(connectionFormat, dbUsername, dbPassword, dbHost, dbPort, dbSchema))
+		if err != nil {
+			// container not ready ... return error to try again
+			return err
+		}
+		return db.Ping()
+	})
+}
+
+func (d *dockerRuntime) tailLogs(containerID string, out io.Writer) {
+	opts := docker.LogsOptions{
+		Context: context.Background(),
+
+		Stderr:      true,
+		Stdout:      true,
+		Follow:      true,
+		Timestamps:  true,
+		RawTerminal: true,
+
+		Container: containerID,
+
+		OutputStream: out,
+	}
+
+	// show the logs on a different thread
+	go func(d *dockerRuntime) {
+		err := d.pool.Client.Logs(opts)
+		if err != nil {
+			log.Errorf("could not forward container logs to write %v", err)
+		}
+	}(d)
+}
+
+func (d *dockerRuntime) tearDownContainerSync() error {
+	return d.pool.Purge(d.sql)
 }
--- go.mod
+++ go.mod
@@ -1,30 +1,43 @@
 module github.com/beatlabs/patron
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/Shopify/sarama v1.21.0
 	github.com/aws/aws-sdk-go v1.21.8
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
+	github.com/containerd/continuity v0.0.0-20200228182428-0f16d7a0959c // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/elastic/go-elasticsearch/v8 v8.0.0-20190731061900-ea052088db25
 	github.com/go-redis/redis/v7 v7.0.0-beta.5
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.3.2
 	github.com/google/uuid v1.1.0
+	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
 	github.com/hashicorp/golang-lru v0.5.3
 	github.com/julienschmidt/httprouter v1.2.0
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/opentracing-contrib/go-stdlib v0.0.0-20180313041242-367231351874
 	github.com/opentracing/opentracing-go v0.0.0-20180606204148-bd9c31933947
+	github.com/ory/dockertest v3.3.5+incompatible
 	github.com/prometheus/client_golang v0.9.1
 	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4
 	github.com/prometheus/common v0.2.0 // indirect
 	github.com/prometheus/procfs v0.0.0-20190129233650-316cf8ccfec5 // indirect
 	github.com/rs/zerolog v1.5.0
 	github.com/streadway/amqp v0.0.0-20180315184602-8e4aba63da9f
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.4.0
 	github.com/uber-go/atomic v1.3.2 // indirect
 	github.com/uber/jaeger-client-go v2.14.0+incompatible
 	github.com/uber/jaeger-lib v1.5.0
 	go.uber.org/atomic v1.3.2 // indirect
 	google.golang.org/grpc v1.27.1
+	gotest.tools v2.2.0+incompatible // indirect
 )
 
 go 1.13
